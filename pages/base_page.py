74a337202572ffac79db79d6640d87a64008371c
Varvara Yurchenko <lemonjazzyurchenko@yandex.ru>
2025-08-05 14:38:13 +0300

    Добавлены адреса в файл curl

---
+++ pages/base_page.py
@@ -0,0 +1,15 @@
+import pytest
+import allure
+from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+class BasePage:
+    def __init__(self, driver):
+        self.driver = driver
+
+    @allure.step("Подождать видимость элемента")
+    def wait_for_element(self, locator, timeout=10):
+        return WebDriverWait(self.driver, timeout).until(EC.visibility_of_element_located(locator))
+
